24abf00fdd5a5312eaef366fb9ec59f16118b506
MSanbira <myt.comp@gmail.com>
2018-08-21 16:14:32 +0300

    first upload

---
+++ model.js
@@ -0,0 +1,18 @@
+let model = {
+    container: {
+        topEdge: 35,
+        bottomEdge: 270,
+        rows: {
+            1: {
+                topEdge: 35,
+                bottomEdge: 65,
+                blocks: [
+                    {
+                        left: 35,
+                        right: 95
+                    }
+                ]
+            }
+        }
+    }
+}
